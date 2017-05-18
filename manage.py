d22f22e8e57d397d19fb5cff8d8223c5351cf24f
fredliu168 <fred@qzcool.com>
2017-05-18 16:34:18 +0800

    Procfile

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 from app import create_app
 from flask_script import Manager
-
+import os
 app = create_app('default')
 
 manager = Manager(app)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run('0.0.0.0', port=port, threaded=True)
