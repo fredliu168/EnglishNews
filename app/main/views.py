fde1b51ab09ae25d3473de86f195ca86a4bd6eca
fredliu168 <fred@qzcool.com>
2017-06-05 10:20:46 +0800

    过滤url特殊字符,处理页面打不开问题

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
+
 from . import main
 
 
 @main.route('/new/<int:newid>/<string:title>/<string:source>')
-def new_content(newid,title,source):
-    return render_template('new-content.html', newid=newid,newTitle = title,Source=source)
+def new_content(newid, title, source):
+
+    return render_template('new-content.html', newid=newid, newTitle=title, Source=source)
 
 
 @main.route('/')
